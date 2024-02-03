13f669a65a79705ee0cb75c41ca1eda472de9391
JavedAkhtar2002 <javedsahil2002@gmail.com>
2024-02-03 22:10:04 +0530

    test

--- test1.c
+++ test1.c
@@ -7,13 +7,14 @@ int diff(int a, int b)
 {
     return a-b;
 }
-<<<<<<< HEAD
 int div(int a, int b)
 {
-    return a/b;
-=======
+    return a/b;}
 int mul(int a, int b)
 {
     return a*b;
->>>>>>> b1
+}
+
+void test(){
+    printf("test")
 }
